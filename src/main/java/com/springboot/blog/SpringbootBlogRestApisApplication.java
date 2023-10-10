442074b226d0aaef291df47a81714ce86211362a
Vishwajeet <singhvishwajeet1998@gmail.com>
2023-10-10 23:01:05 +0530

    Added Post Entity Code

---
+++ src/main/java/com/springboot/blog/SpringbootBlogRestApisApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBlogRestApisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBlogRestApisApplication.class, args);
+	}
+
+}
